3bf8d66fbe8422907121b0bc5fca4d10087cf123
YusofBandar <yusof.bandar@squaredup.com>
2020-08-02 22:32:09 +0100

    Adds Slider component

---
+++ trajectory-graph/src/common/slider/Slider.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Slider.module.css';
+
+/**
+ * Slider
+ */
+function Slider({ labels, value, onChange, inputProps }) {
+    return (
+        <div className={ styles.sliderWrapper }>
+          <input
+            className={ styles.slider }
+            value={ value }
+            onChange={ onChange }
+            type='range' { ...inputProps }/>
+          { labels && labels.map(({ label, point }) => (
+              <span style={{ left: `${point}px` }} className={ styles.label }>{ label }</span>
+          ))}
+        </div>
+    )
+};
+
+Slider.propTypes = {
+    labels: PropTypes.arrayOf(PropTypes.shape({
+        label: PropTypes.string.isRequired,
+        point: PropTypes.number.isRequired
+    })),
+    value: PropTypes.number.isRequired,
+    onChange: PropTypes.func.isRequired
+};
+
+
+export default Slider;
