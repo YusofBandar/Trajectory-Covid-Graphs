470eb8c5319fd5f71da1ae481a82b1c0822ea53c
YusofBandar <yusof.bandar@squaredup.com>
2020-08-02 22:32:40 +0100

    Adds slider to US Map

--- trajectory-graph/src/ui/us-map/USMap.js
+++ trajectory-graph/src/ui/us-map/USMap.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import states from './states'
 
 import Map from '../../common/map/Map';
+import Slider from '../../common/slider/Slider';
 
 import styles from './USMap.module.css';
 
@@ -10,7 +11,8 @@ import styles from './USMap.module.css';
  * USMap
  */
 function USMap({ title, data }) {
-    let stateData = data.map(d => {
+    const [date, setDate] = useState(50);
+    const stateData = data.map(d => {
         const meta = states[d.state];
         return {
             ...d,
@@ -19,10 +21,17 @@ function USMap({ title, data }) {
         };
     });
 
+    const handleDateChange = (event) => {
+        setDate(Number(event.target.value));
+    };
+
     return (
         <div className={ styles.map }>
           <h1 className={ styles.title }>{ title }</h1>
           <Map data={ stateData }/>
+          <div className={ styles.slider }>
+            <Slider value={ date } onChange={ handleDateChange }/>
+          </div>
         </div>
     );
 };
