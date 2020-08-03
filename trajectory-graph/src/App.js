b0db250cf6abb186cc407dc09fe1746c33928543
YusofBandar <yusof.bandar@squaredup.com>
2020-08-03 23:30:51 +0100

    Adds Fetch hook to get state data

--- trajectory-graph/src/App.js
+++ trajectory-graph/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import USMap from './ui/us-map/USMap';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const [isLoading, response] = useFetch('https://covidtracking.com/api/v1/states/daily.json');
+    return (
+        <div className="App">
+          { !isLoading && <USMap title='US Data' data={ response }/> }
+        </div>
+    );
 }
 
 export default App;
+
+function useFetch(url, options) {
+    const [isLoading, setLoading] = useState(true);
+    const [response, setResponse] = useState({});
+
+    useEffect(() => {
+        const request = async () => {
+            setLoading(true);
+            const res = await fetch(url, options);
+            const json = await res.json();
+            setResponse(json);
+            setLoading(false);
+        }
+
+        request();
+    }, [url, options]);
+
+    return [isLoading, response];
+}
