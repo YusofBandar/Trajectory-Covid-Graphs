2cf6676a744c5c09213fbe27dd17594ccd0abf5a
YusofBandar <yusof.bandar@squaredup.com>
2020-08-08 22:30:42 +0100

    Uses new Month Scale Component

--- trajectory-graph/src/ui/trajectories-states/TrajectoriesStates.js
+++ trajectory-graph/src/ui/trajectories-states/TrajectoriesStates.js
@@ -1,33 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react';
-import * as d3 from 'd3';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import StatesService from '../../services/statesService';
 import states from './states'
 
-import Play from '../../img/play.svg';
-import Pause from '../../img/pause.svg';
-
 import Map from '../../common/map/Map';
-import Slider from '../../common/slider/Slider';
 import Scale from '../../common/scale/Scale';
+import MonthSlider from '../month-slider/MonthSlider';
 
 import styles from './TrajectoriesStates.module.css';
-
-const useTimer = (callback, interval, inital) => {
-    const timer = useRef();
-
-    const [play, setPlay] = useState(inital);
-    useEffect(() => {
-        if(play){
-            typeof timer.current === 'object' && timer.current.stop();
-            timer.current = d3.interval(callback, interval);
-        }else{
-            typeof timer.current === 'object' && timer.current.stop();
-        }
-    }, [timer, callback, interval, play]);
-
-    return [play, setPlay];
-};
 
 /**
  * TrajectoriesStates
@@ -63,13 +43,7 @@ function TrajectoriesStates({ title, data, dimension, maxValue }) {
     })
 
     const scale = stateData.scale;
-
     const dataLength = stateData.groups.values().next().value.length;
-
-    const [play, setPlay] = useTimer(() => {
-        setDate(date => date < dataLength - 1 ? date + 1 : date);
-    }, 500, false);
-
 
     const currentPoint = [];
     getStates(stateData.groups, date).forEach(state => {
@@ -80,12 +54,9 @@ function TrajectoriesStates({ title, data, dimension, maxValue }) {
         currentPoint.push({ ...state, angle });
     })
 
-    const handleDateChange = (event) => {
-        setDate(Number(event.target.value));
-    };
-
-    const handlePlayClick = () => {
-        setPlay(play => !play);
+    const handleDateChange = (value) => {
+        console.log(value, dataLength);
+        setDate(value);
     };
 
     const labels = StatesService.distributedMonths('Mar', dataLength).map((l, i) => ({
@@ -97,10 +68,7 @@ function TrajectoriesStates({ title, data, dimension, maxValue }) {
         <div className={ styles.map }>
           <h1 className={ styles.title }>{ title }</h1>
           <Map data={ currentPoint }/>
-          <div className={ styles.slider }>
-            <img className={ styles.play } onClick={ handlePlayClick } src={ play ? Pause : Play } alt='play'/>
-            <Slider value={ date } onChange={ handleDateChange } labels={ labels } inputProps={{min: 0, max: dataLength - 5}}/>
-          </div>
+          <MonthSlider value={ date } length={ dataLength - 1 } labels={ labels } onChange={ handleDateChange }/>
           <div className={ styles.scale }>
             <Scale min={ 0 } max={ maxValue }/>
           </div>
