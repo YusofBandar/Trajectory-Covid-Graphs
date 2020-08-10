ac9bdd0db401676e9a675d334730dca4f7251a68
YusofBandar <yusof.bandar@squaredup.com>
2020-08-10 23:21:50 +0100

    Changes scale vis to use Map

--- trajectory-graph/src/ui/scales-states/ScalesStates.js
+++ trajectory-graph/src/ui/scales-states/ScalesStates.js
@@ -1,10 +1,9 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import * as d3 from 'd3';
-import StatesService from '../../services/statesService';
 
+import Map from '../../common/map/Map';
 import Label from '../../common/label/Label';
-import MonthSlider from '../month-slider/MonthSlider';
 
 import styles from './ScalesStates.module.css';
 
@@ -12,8 +11,6 @@ import styles from './ScalesStates.module.css';
  * ScalesStates
  */
 function ScalesStates({ title, data, dimension }) {
-    const [date, setDate] = useState(0);
-    const dataLength = data.values().next().value.length;
     const maxValue = maxDimensionValue(data, dimension);
     const scale = useCallback(
         d3.scaleLinear()
@@ -21,30 +18,26 @@ function ScalesStates({ title, data, dimension }) {
           .range([10, 60])
     );
 
-    const currentPoint = [];
-    getStates(data, date).forEach(state => {
-        const value = state.data[dimension];
-        currentPoint.push({ ...state, scale: scale(value) });
-    })
-
-    const handleDateChange = (value) => {
-        setDate(value);
-    };
-
-    const labels = StatesService.distributedMonths('Mar', dataLength).map((l, i) => ({
-        label: l,
-        point: i * 25
-    }));
-
     return (
         <div className={ styles.map }>
-          <h1 className={ styles.title }>{ title }</h1>
-          <svg className={ styles.states }>
-            { currentPoint.map(d => (
-                <Label key={ d.label } label={ d.data.state } x={ d.x } y={ d.y } scale={ d.scale }/>
-            ))}
-          </svg>
-          <MonthSlider value={ date } length={ dataLength - 1 } labels={ labels } onChange={ handleDateChange }/>
+          <Map title={ title } data={ data }>
+            { (currentPoints) => {
+                const labels = [];
+                currentPoints.forEach(state => {
+                    const value = scale(state.data[dimension]);
+                    labels.push(
+                        <Label
+                          key={ state.label }
+                          label={ state.data.state }
+                          x={ state.x }
+                          y={ state.y }
+                          scale={ value }/>
+                    );
+                })
+
+                return labels;
+            }}
+          </Map>
         </div>
     );
 };
@@ -59,14 +52,6 @@ function maxDimensionValue(map, dimension){
 
     return max;
 }
-
-function getStates (states, index) {
-    let stateTrajectories = [];
-    states.forEach(seriesData => {
-        stateTrajectories.push(seriesData[index]);
-    });
-    return stateTrajectories;
-};
 
 ScalesStates.propTypes = {
     title: PropTypes.string.isRequired,
