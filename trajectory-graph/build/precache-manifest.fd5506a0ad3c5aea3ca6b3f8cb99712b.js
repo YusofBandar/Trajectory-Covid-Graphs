fb6e9d5f53b54a59fe0342c8b4a538cfc80869be
YusofBandar <yusof.bandar@squaredup.com>
2020-08-20 23:04:52 +0100

    Adds build

---
+++ trajectory-graph/build/precache-manifest.fd5506a0ad3c5aea3ca6b3f8cb99712b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759a7eb8fadbca747c81156f903e279d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248cd93edb7ed184d06d",
+    "url": "/static/css/main.cc8f5241.chunk.css"
+  },
+  {
+    "revision": "0747de016cfb711bdd18",
+    "url": "/static/js/2.1ef58fdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ef58fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248cd93edb7ed184d06d",
+    "url": "/static/js/main.3e3a2711.chunk.js"
+  },
+  {
+    "revision": "fdac48054301b0bbf31b",
+    "url": "/static/js/runtime-main.17a84603.js"
+  },
+  {
+    "revision": "c48d680abe2e934095a51b121413fb80",
+    "url": "/static/media/github.c48d680a.svg"
+  },
+  {
+    "revision": "404752206751ff14dca172f21ba9702f",
+    "url": "/static/media/pause.40475220.svg"
+  },
+  {
+    "revision": "221bbeade52b2f0e974886019cb1dda4",
+    "url": "/static/media/play.221bbead.svg"
+  },
+  {
+    "revision": "50cf6ea37980053afe2290bc4ad73249",
+    "url": "/static/media/twitter.50cf6ea3.svg"
+  }
+]);
