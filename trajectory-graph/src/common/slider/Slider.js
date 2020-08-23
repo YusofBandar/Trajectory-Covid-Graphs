13a41083143d2fec7d84d4dcd81e81eaf6cb8eca
Yusof Bandar <yusof.bandar@int.squaredup.com>
2020-08-23 23:27:41 +0100

    Restyles Map border

--- trajectory-graph/src/common/slider/Slider.js
+++ trajectory-graph/src/common/slider/Slider.js
@@ -9,18 +9,18 @@ import styles from './Slider.module.css';
 function Slider({ labels, valueLabel, value, min, max, onChange, inputProps }) {
     return (
         <div className={ styles.sliderWrapper }>
-          { valueLabel && <span className={ styles.valueLabel } style={{ left: `${(value / max) * 100}%` }}>{ valueLabel }</span> }
-          <input
-            className={ styles.slider }
-            value={ value }
-            min={ min }
-            max={ max }
-            onChange={ onChange }
-            type='range'
-            { ...inputProps }/>
-          { labels && labels.map(({ label, point }) => (
-              <span style={{ left: `${point}%` }} className={ styles.label }>{ label }</span>
-          ))}
+            { valueLabel && <span className={ styles.valueLabel } style={{ left: `${(value / max) * 100}%` }}>{ valueLabel }</span> }
+            <input
+                className={ styles.slider }
+                value={ value }
+                min={ min }
+                max={ max }
+                onChange={ onChange }
+                type='range'
+                { ...inputProps }/>
+            { labels && labels.map(({ label, point }) => (
+                <span style={{ left: `${point}%` }} className={ styles.label }>{ label }</span>
+            ))}
         </div>
     )
 };
